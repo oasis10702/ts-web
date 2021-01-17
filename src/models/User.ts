16d243930e4da0cfdae779567b9c77f1e1762cef
oasis10702 <oasis10702@gmail.com>
2021-01-17 19:21:57 +0900

    storing event listener

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,6 +5,8 @@ interface UserProps {
 
 type Callback = () => {};
 export class User {
+  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
