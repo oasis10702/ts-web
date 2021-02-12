afe34853bcd7d6b4668f61cb6ba00c64caa916dc
oasis10702 <oasis10702@gmail.com>
2021-02-12 22:19:19 +0900

    delete

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 import { User } from './models/User';
 
-const user = new User();
-
-user.events.on('change', () => {
-  console.log('change!');
-});
-
-user.events.trigger('change');
+const user = new User({name: 'new recoard', age: 0});
