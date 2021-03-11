82e5947932657933f5790b7aacfd0721a8a3e725
oasis10702 <oasis10702@gmail.com>
2021-03-11 23:01:27 +0900

    bind events

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -3,12 +3,12 @@ export class UserForm {
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      "click:button": this.onButtonClick,
+      'click:button': this.onButtonClick
     };
   }
 
   onButtonClick(): void {
-    console.log("hi there");
+    console.log('hi there');
   }
 
   template(): string {
@@ -21,9 +21,23 @@ export class UserForm {
     `;
   }
 
+  bindEvents(fragment: DocumentFragment) {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
   render(): void {
-    const templateElement = document.createElement("template");
+    const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
