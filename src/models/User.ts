8edb871909f1536d489a9e882464af1328d0f7d4
oasis10702 <oasis10702@gmail.com>
2021-03-14 23:36:22 +0900

    add methods to user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,8 +1,8 @@
-import { Model } from "./Model";
-import { Attributes } from "./Attributes";
-import { ApiSync } from "./ApiSync";
-import { Eventing } from "./Eventing";
-import { Collection } from "./Collection";
+import { Model } from './Model';
+import { Attributes } from './Attributes';
+import { ApiSync } from './ApiSync';
+import { Eventing } from './Eventing';
+import { Collection } from './Collection';
 
 export interface UserProps {
   id?: number;
@@ -10,21 +10,19 @@ export interface UserProps {
   age?: number;
 }
 
-const rootUrl = "http://localhost:3000/users";
+const rootUrl = 'http://localhost:3000/users';
 
 export class User extends Model<UserProps> {
   static buildUser(attrs: UserProps): User {
-    return new User(
-      new Attributes(attrs),
-      new Eventing(),
-      new ApiSync<UserProps>(rootUrl)
-    );
+    return new User(new Attributes(attrs), new Eventing(), new ApiSync<UserProps>(rootUrl));
   }
 
   static buildUserCollection(): Collection<User, UserProps> {
-    return new Collection<User, UserProps>(
-      rootUrl,
-      (json: UserProps) => User.buildUser(json)
-    );
+    return new Collection<User, UserProps>(rootUrl, (json: UserProps) => User.buildUser(json));
+  }
+
+  setRandomAge(): void {
+    const age = Math.round(Math.random() * 100);
+    this.set({ age });
   }
 }
