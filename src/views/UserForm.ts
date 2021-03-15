edcf43a09e893f1a2c699ee7d0be1d17ef85afd9
oasis10702 <oasis10702@gmail.com>
2021-03-15 19:17:25 +0900

    render on model change

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,11 +1,19 @@
-import { User } from '../models/User';
+import { User } from "../models/User";
 
 export class UserForm {
-  constructor(public parent: Element, public model: User) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on("change", () => {
+      this.render();
+    });
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:.set-age': this.onSetAgeClick
+      "click:.set-age": this.onSetAgeClick,
     };
   }
 
@@ -17,8 +25,8 @@ export class UserForm {
     return `
       <div>
         <h1>User Form</h1>
-        <div>User name: ${this.model.get('name')}</div>
-        <div>User age: ${this.model.get('age')}</div>
+        <div>User name: ${this.model.get("name")}</div>
+        <div>User age: ${this.model.get("age")}</div>
         <input />
         <button>Click Me</button>
         <button class="set-age">Set Random Age</button>
@@ -30,16 +38,18 @@ export class UserForm {
     const eventsMap = this.eventsMap();
 
     for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(':');
+      const [eventName, selector] = eventKey.split(":");
 
-      fragment.querySelectorAll(selector).forEach(element => {
+      fragment.querySelectorAll(selector).forEach((element) => {
         element.addEventListener(eventName, eventsMap[eventKey]);
       });
     }
   }
 
   render(): void {
-    const templateElement = document.createElement('template');
+    this.parent.innerHTML = "";
+
+    const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
 
     this.bindEvents(templateElement.content);
