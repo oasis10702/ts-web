6a5d44f7d41cd8f2b395ce2e120c1f0c26c04362
oasis10702 <oasis10702@gmail.com>
2021-03-16 21:22:36 +0900

    read input text'

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,4 +1,4 @@
-import { User } from "../models/User";
+import { User } from '../models/User';
 
 export class UserForm {
   constructor(public parent: Element, public model: User) {
@@ -6,16 +6,23 @@ export class UserForm {
   }
 
   bindModel(): void {
-    this.model.on("change", () => {
+    this.model.on('change', () => {
       this.render();
     });
   }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      "click:.set-age": this.onSetAgeClick,
+      'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick
     };
   }
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+    const name = input.value;
+    this.model.set({ name });
+  };
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
@@ -25,10 +32,10 @@ export class UserForm {
     return `
       <div>
         <h1>User Form</h1>
-        <div>User name: ${this.model.get("name")}</div>
-        <div>User age: ${this.model.get("age")}</div>
+        <div>User name: ${this.model.get('name')}</div>
+        <div>User age: ${this.model.get('age')}</div>
         <input />
-        <button>Click Me</button>
+        <button class="set-name">Change Name</button>
         <button class="set-age">Set Random Age</button>
       </div>
     `;
@@ -38,18 +45,18 @@ export class UserForm {
     const eventsMap = this.eventsMap();
 
     for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(":");
+      const [eventName, selector] = eventKey.split(':');
 
-      fragment.querySelectorAll(selector).forEach((element) => {
+      fragment.querySelectorAll(selector).forEach(element => {
         element.addEventListener(eventName, eventsMap[eventKey]);
       });
     }
   }
 
   render(): void {
-    this.parent.innerHTML = "";
+    this.parent.innerHTML = '';
 
-    const templateElement = document.createElement("template");
+    const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
     this.bindEvents(templateElement.content);
