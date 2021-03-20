85f25d0bb4a9d67bf3e19084a0d3024feed71c60
oasis10702 <oasis10702@gmail.com>
2021-03-20 23:10:48 +0900

    save data from view

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,15 +1,20 @@
-import { User, UserProps } from "../models/User";
-import { View } from "./View";
+import { User, UserProps } from '../models/User';
+import { View } from './View';
 export class UserForm extends View<User, UserProps> {
   eventsMap(): { [key: string]: () => void } {
     return {
-      "click:.set-age": this.onSetAgeClick,
-      "click:.set-name": this.onSetNameClick,
+      'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick
     };
   }
 
+  onSaveClick = (): void => {
+    this.model.save();
+  };
+
   onSetNameClick = (): void => {
-    const input = this.parent.querySelector("input");
+    const input = this.parent.querySelector('input');
 
     if (input) {
       const name = input.value;
@@ -24,12 +29,10 @@ export class UserForm extends View<User, UserProps> {
   template(): string {
     return `
       <div>
-        <h1>User Form</h1>
-        <div>User name: ${this.model.get("name")}</div>
-        <div>User age: ${this.model.get("age")}</div>
-        <input />
+        <input palceholder="${this.model.get('name')}" />
         <button class="set-name">Change Name</button>
         <button class="set-age">Set Random Age</button>
+        <button class="save-model">Save User</button>
       </div>
     `;
   }
