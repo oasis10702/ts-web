54fee61bc96535aa80dddcea8ab49edba6eeae66
oasis10702 <oasis10702@gmail.com>
2021-03-21 21:30:34 +0900

    add user show and edit

--- src/views/View.ts
+++ src/views/View.ts
@@ -1,15 +1,18 @@
-import { Model } from "../models/Model";
+import { Model } from '../models/Model';
 
 export abstract class View<T extends Model<K>, K> {
   constructor(public parent: Element, public model: T) {
     this.bindModel();
   }
 
-  abstract eventsMap(): { [key: string]: () => void };
   abstract template(): string;
 
+  eventsMap(): { [key: string]: () => void } {
+    return {};
+  }
+
   bindModel(): void {
-    this.model.on("change", () => {
+    this.model.on('change', () => {
       this.render();
     });
   }
@@ -18,18 +21,18 @@ export abstract class View<T extends Model<K>, K> {
     const eventsMap = this.eventsMap();
 
     for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(":");
+      const [eventName, selector] = eventKey.split(':');
 
-      fragment.querySelectorAll(selector).forEach((element) => {
+      fragment.querySelectorAll(selector).forEach(element => {
         element.addEventListener(eventName, eventsMap[eventKey]);
       });
     }
   }
 
   render(): void {
-    this.parent.innerHTML = "";
+    this.parent.innerHTML = '';
 
-    const templateElement = document.createElement("template");
+    const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
     this.bindEvents(templateElement.content);
