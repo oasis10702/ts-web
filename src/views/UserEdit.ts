e2d320f81162c99f485cb5ac8ee2f3ddfe7553b9
oasis10702 <oasis10702@gmail.com>
2021-03-21 21:41:54 +0900

    fix

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -4,10 +4,10 @@ import { User, UserProps } from '../models/User';
 export class UserEdit extends View<User, UserProps> {
   template(): string {
     return `
-        <div>
-          <div class="user-show"></div>
-          <div class="user-form"></div>
-        </div>
-      `;
+      <div>
+        <div class="user-show"></div>
+        <div class="user-form"></div>
+      </div>
+    `;
   }
 }
