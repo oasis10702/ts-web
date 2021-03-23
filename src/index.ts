1ba90fa6234d16a6bd304163a884c0b6a7aadf39
oasis10702 <oasis10702@gmail.com>
2021-03-23 22:58:03 +0900

    ifx

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,7 @@ const root = document.getElementById('root');
 
 if (root) {
   const userEdit = new UserEdit(root, user);
-
   userEdit.render();
-  console.log(userEdit);
 } else {
   throw new Error('root element not found');
 }
