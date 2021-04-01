6bdcefc6b0e2fe8f30d6c7a96a83c98e8ad3bf43
oasis10702 <oasis10702@gmail.com>
2021-04-01 21:08:46 +0900

    fix

--- src/views/UserList.ts
+++ src/views/UserList.ts
@@ -2,5 +2,5 @@ import { CollectionView } from './CollectionView';
 import { User, UserProps } from '../models/User';
 
 export class UserList extends CollectionView<User, UserProps> {
-  /** TODO */
+  // TODO
 }
