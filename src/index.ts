e14b5c22623f9a4677449ca2bd7f499630a8d324
oasis10702 <oasis10702@gmail.com>
2021-04-03 20:59:17 +0900

    collection view end

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
 
-const user = User.buildUser({ name: 'NAME', age: 20 });
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+  return User.buildUser(json);
+});
 
-const root = document.getElementById('root');
+users.on('change', () => {
+  const root = document.getElementById('root');
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-} else {
-  throw new Error('root element not found');
-}
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
